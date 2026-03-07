56e4f2775ee4a2f0d0e45db0b420aadca6054544
Akshay <akshaykocharekar3@gmail.com>
2026-03-08 02:51:52 +0530

    config changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://easy-commute-one.vercel.app",
+    ],
     credentials: true,
   })
 );
