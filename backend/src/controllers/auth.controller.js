47b6fdf677074dbec70d447d18fdd9715ebc448a
Akshay kocharekar <akshaykocharekar3@gmail.com>
2026-03-31 22:36:30 +0530

    temp auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -164,7 +164,7 @@ exports.login = async (req, res) => {
     if (!isMatch) return res.status(401).json({ message: "Invalid credentials" });
 
     // Operator gets OTP on every login
-    if (user.role === "BUS_OPERATOR") {
+   /* if (user.role === "BUS_OPERATOR") {
       const otp = generateOTP();
       user.otp = otp;
       user.otpExpiresAt = new Date(Date.now() + 10 * 60 * 1000);
@@ -195,7 +195,7 @@ exports.login = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
-
+*/
 // VERIFY OPERATOR LOGIN OTP
 exports.verifyLoginOTP = async (req, res) => {
   try {
